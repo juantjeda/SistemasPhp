cbd10e7d14c7aba78e72c150997ac7a67e9a3c52
Juan Jose Tejeda Hernandez <juantjeda@gmail.com>
2013-02-06 16:59:42 -0600

    nueva forma

--- inicio.php
+++ inicio.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+session_start();
+?>
 <html lang="es">
 <head>
 	<META charset="utf-8"/>
@@ -6,22 +9,22 @@
 </head>
 <body>
 	<h1>Validacion de Usuario</h1>
-	<?php 
-		if($_GET['error_usuario'] =="Si"){
-			?>
-			<p>Datos Incorrectos.</p>
-			<?php
-		}
-		else{
-			?>
-			<p>Introduce tu usuario y password</p>
-			<?php
-		}
+	<?php
+	if(isset($_POST['enviar'])){
+		if(empty($_POST['usuario']) || empty($_POST['password']))
+			echo 'Debes de llenar todos los datos';
+		else
+			if($_POST['usuario'] ==TRUE and $_POST['password'] ==TRUE){
+				$_SESSION['usuario'] = $_POST['usuario'];
+				$_SESSION['password'] = $_POST['password'];	
+				echo 'Te has logeado como '.$_SESSION['usuario'];
+			}
+	}
 	?>
-	<form action="autentica.php" method="POST">
+	<form action="inicio.php" method="POST">
 		<p>Usuario: <input type="text" name="usuario"/></p>
-		<p>Password: <input type="text" name="password"/></p>
-		<input type="submit"/>
+		<p>Password: <input type="password" name="password"/></p>
+		<input type="submit" name="enviar"/>
 	</form>
 </body>
 </html>
